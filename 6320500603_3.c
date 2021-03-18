305e5bddab4d4c8633f6156a029f8312819eea98
icephat <phattaraporn.sa@ku.th>
2021-03-18 17:27:21 +0700

    เพิ่มโค้ดการรับค่า

--- 6320500603_3.c
+++ 6320500603_3.c
@@ -5,7 +5,17 @@ int main()
     scanf("%d",&n);
     int a[n][2],b[n],k,x[n];
 
-
+    if(n>1&&n<1001)
+    {
+        for(i=0; i<n; i++)
+        {
+            b[i]=i+1;
+            for(j=0; j<2; j++)
+            {
+                scanf("%d",&a[i][j]);
+            }
+        }
+    }
 
 
 

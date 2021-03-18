d2e4958e1bbb6f530c22515e133df7fb6563891a
icephat <phattaraporn.sa@ku.th>
2021-03-18 15:24:28 +0700

    เพิ่มรูปแบบและการรับค่า

---
+++ 6320500603_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    char a[n];
+    char b[n][100];
+    char temp[100];
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",&a[i]);
+    }
+
+
+
+
+
+
+    return 0;
+}

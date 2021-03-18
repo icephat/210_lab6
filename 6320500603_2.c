58bcaec18fe6fde788f8d009e56aa0780a852924
icephat <phattaraporn.sa@ku.th>
2021-03-18 16:11:54 +0700

    เพิ่มโค้ด แต่ผิด1กรณี ค่อยกลับมาคิดค่ะ

---
+++ 6320500603_2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,first,rd,k=0,x=0;
+    scanf("%d",&n);
+    int a[n];
+    int b[n];
+    scanf("%d",&first);
+    scanf("%d",&rd);
+    for(i=0;i<n;i++)
+    {
+        a[i]=first%10;
+        first/=10;
+        b[i]=rd%10;
+        rd-=10;
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]==b[i])
+        {
+            k++;
+        }
+        else
+        {
+            x++;
+        }
+    }
+    printf("%d %d",k,x);
+
+
+
+
+
+
+
+    return 0;
+}

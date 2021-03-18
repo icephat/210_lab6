a8016185be6b4ff7a299788c0c8165f90c7f3ab7
icephat <phattaraporn.sa@ku.th>
2021-03-18 15:48:12 +0700

    แก้ไขและเพิ่มโค้ด

--- 6320500603_1.c
+++ 6320500603_1.c
@@ -1,17 +1,34 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    char a[n];
+
     char b[n][100];
-    char temp[100];
+    char c[100];
     for(i=0;i<n;i++)
     {
-        scanf("%s",&a[i]);
+        scanf("%s",b[i]);
     }
 
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(strcmp(b[j],b[j+1])==1)
+            {
+                strcpy(c,b[j]);
+                strcpy(b[j],b[j+1]);
+                strcpy(b[j+1],c);
+            }
 
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%s\n",b[i]);
+    }
 
 
 
